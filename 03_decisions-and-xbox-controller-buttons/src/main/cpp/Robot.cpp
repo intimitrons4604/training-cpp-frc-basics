7b7a85826e84944c43e9239a0695b5e773d992d6
pgfisico <pgfisico.dev@gmail.com>
2018-12-10 23:55:42 -0700

    Add decisions example

---
+++ 03_decisions-and-xbox-controller-buttons/src/main/cpp/Robot.cpp
@@ -0,0 +1,51 @@
+#include "Robot.h"
+
+void Robot::RobotInit()
+{
+    // Intentionally empty
+}
+
+void Robot::RobotPeriodic()
+{
+    // Intentionally empty
+}
+
+void Robot::AutonomousInit()
+{
+    // Intentionally empty
+}
+
+void Robot::AutonomousPeriodic()
+{
+    // Intentionally empty
+}
+
+void Robot::TeleopInit()
+{
+    // Intentionally empty
+}
+
+void Robot::TeleopPeriodic()
+{
+    // TODO Add code to spin the motor here
+}
+
+void Robot::TestInit()
+{
+    // Intentionally empty
+}
+
+void Robot::TestPeriodic()
+{
+    // Intentionally empty
+}
+
+void Robot::DisabledInit()
+{
+    // Intentionally empty
+}
+
+void Robot::DisabledPeriodic()
+{
+    // Intentionally empty
+}
