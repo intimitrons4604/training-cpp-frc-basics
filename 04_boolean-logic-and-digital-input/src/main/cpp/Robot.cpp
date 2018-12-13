fff987a6e650ae9deec956de797f17d7269efced
pgfisico <pgfisico.dev@gmail.com>
2018-12-12 21:11:38 -0700

    Add boolean logic lesson

---
+++ 04_boolean-logic-and-digital-input/src/main/cpp/Robot.cpp
@@ -0,0 +1,51 @@
+#include "Robot.h"
+
+void Robot::RobotInit()
+{
+    // Intentionally empty
+}
+
+void Robot::RobotPeriodic()
+{
+    // Intentionally empty
+}
+
+void Robot::AutonomousInit()
+{
+    // Intentionally empty
+}
+
+void Robot::AutonomousPeriodic()
+{
+    // TODO Add code to spin the motor only if the limit switch is not pressed
+}
+
+void Robot::TeleopInit()
+{
+    // Intentionally empty
+}
+
+void Robot::TeleopPeriodic()
+{
+    // TODO Add code to spin the motor only if the A button is pressed and the limit switch is not pressed
+}
+
+void Robot::TestInit()
+{
+    // Intentionally empty
+}
+
+void Robot::TestPeriodic()
+{
+    // Intentionally empty
+}
+
+void Robot::DisabledInit()
+{
+    // Intentionally empty
+}
+
+void Robot::DisabledPeriodic()
+{
+    // Intentionally empty
+}
