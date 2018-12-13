ac2b8b55e87572b3e2ad5b056f61846dbdc69181
pgfisico <pgfisico.dev@gmail.com>
2018-12-12 22:09:09 -0700

    Add axial deadzone exercise

---
+++ 08_motor-control-axial-deadzone/src/main/cpp/Robot.cpp
@@ -0,0 +1,57 @@
+#include "Robot.h"
+
+void Robot::RobotInit()
+{
+    // Intentionally empty
+}
+
+void Robot::RobotPeriodic()
+{
+    // Intentionally empty
+}
+
+void Robot::AutonomousInit()
+{
+    // Intentionally empty
+}
+
+void Robot::AutonomousPeriodic()
+{
+    // Intentionally empty
+}
+
+void Robot::TeleopInit()
+{
+    // Intentionally empty
+}
+
+void Robot::TeleopPeriodic()
+{
+    // TODO Add code to spin the motor forward or backwards at a speed
+    // linearly proportional to how far the right stick is pushed forward
+    // or backwards along the y axis, with the following caveat.
+    //
+    // If the stick is less than 10% deflected in either direction,
+    // stop the motor. Otherwise, spin the motor forward or backward
+    // at a linearly proportional speed as normal.
+}
+
+void Robot::TestInit()
+{
+    // Intentionally empty
+}
+
+void Robot::TestPeriodic()
+{
+    // Intentionally empty
+}
+
+void Robot::DisabledInit()
+{
+    // Intentionally empty
+}
+
+void Robot::DisabledPeriodic()
+{
+    // Intentionally empty
+}
