34223758732537975aa8503c89fd8c3bddc1cd54
pgfisico <pgfisico.dev@gmail.com>
2018-12-12 21:39:09 -0700

    Add output limiting exercise

---
+++ 07_motor-control-output-limit/src/main/cpp/Robot.cpp
@@ -0,0 +1,59 @@
+#include "Robot.h"
+
+typedef frc::GenericHID::JoystickHand JoystickHand;
+
+void Robot::RobotInit()
+{
+    // Intentionally empty
+}
+
+void Robot::RobotPeriodic()
+{
+    // Intentionally empty
+}
+
+void Robot::AutonomousInit()
+{
+    // Intentionally empty
+}
+
+void Robot::AutonomousPeriodic()
+{
+    // Intentionally empty
+}
+
+void Robot::TeleopInit()
+{
+    // Intentionally empty
+}
+
+void Robot::TeleopPeriodic()
+{
+    // Note that the typedef at the top of the file saves us from having to type out the fully qualified JoystickHand
+    double rightStickY = xboxController.GetY(JoystickHand::kRightHand);
+
+    // TODO Add code to spin the motor forward or backwards at a speed
+    // linearly proportional to how far the right stick is pushed forward
+    // or backwords along the y axis (rightStickY), but limit the maximum
+    // motor power in both directions to 60%
+}
+
+void Robot::TestInit()
+{
+    // Intentionally empty
+}
+
+void Robot::TestPeriodic()
+{
+    // Intentionally empty
+}
+
+void Robot::DisabledInit()
+{
+    // Intentionally empty
+}
+
+void Robot::DisabledPeriodic()
+{
+    // Intentionally empty
+}
