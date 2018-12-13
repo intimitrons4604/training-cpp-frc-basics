d07397a7d99bfb50ee9d3e774cd2d158837a1f4e
pgfisico <pgfisico.dev@gmail.com>
2018-12-12 22:14:28 -0700

    Add dual motor passthrough exercise

---
+++ 09_analog-stick-dual-motor-control/src/main/cpp/Robot.cpp
@@ -0,0 +1,53 @@
+#include "Robot.h"
+
+void Robot::RobotInit()
+{
+    // Intentionally empty
+}
+
+void Robot::RobotPeriodic()
+{
+    // Intentionally empty
+}
+
+void Robot::AutonomousInit()
+{
+    // Intentionally empty
+}
+
+void Robot::AutonomousPeriodic()
+{
+    // Intentionally empty
+}
+
+void Robot::TeleopInit()
+{
+    // Intentionally empty
+}
+
+void Robot::TeleopPeriodic()
+{
+    // TODO Add code to spin both motors forward or backwards at a speed
+    // linearly proportional to stick deflection along the y axis, with the
+    // left stick controlling one motor and the right stick controlling the other
+}
+
+void Robot::TestInit()
+{
+    // Intentionally empty
+}
+
+void Robot::TestPeriodic()
+{
+    // Intentionally empty
+}
+
+void Robot::DisabledInit()
+{
+    // Intentionally empty
+}
+
+void Robot::DisabledPeriodic()
+{
+    // Intentionally empty
+}
---
+++ 09_analog-stick-dual-motor-control/src/main/include/Robot.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Talon.h>
+#include <TimedRobot.h>
+#include <XboxController.h>
+
+class Robot : public frc::TimedRobot
+{
+public:
+  void RobotInit() override;
+
+  void RobotPeriodic() override;
+
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+
+  void TestInit() override;
+  void TestPeriodic() override;
+
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+
+private:
+  frc::Talon motor{0};
+  // TODO Replace this line by adding a second motor controller to the robot
+  frc::XboxController xboxController{0};
+};
