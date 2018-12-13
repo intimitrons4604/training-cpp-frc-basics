2cc91e6a2e6785ed4e04781fd61c8ac1d20c2637
pgfisico <pgfisico.dev@gmail.com>
2018-12-12 21:20:22 -0700

    Add xbox trigger lesson

---
+++ 05_xbox-controller-trigger-motor-control/src/main/include/Robot.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Talon.h>
+#include <TimedRobot.h>
+#include <XboxController.h>
+
+class Robot : public frc::TimedRobot
+{
+public:
+  void RobotInit() override;
+
+  void RobotPeriodic() override;
+
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+
+  void TestInit() override;
+  void TestPeriodic() override;
+
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+
+private:
+  frc::Talon motor{0};
+  frc::XboxController xboxController{0};
+};
